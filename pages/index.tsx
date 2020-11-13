4a20476cf91db0a4acc86c8a486b4ba0b422c277
PepijnSenders <ps@hellofresh.com>
2020-11-13 16:04:19 -0500

    Add cache

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,9 @@ const Home: React.FC<{ hello: string }> = ({ hello }) => {
     return <h1>Hello {hello}</h1>
 };
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async ({ res }) => {
+    res.setHeader('Cache-Control', 'public, max-age=7200')
+
     return {
         props: {
             hello: 'world',
